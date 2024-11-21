5bc1dc8059ef52a9ede50650f4f9b4c72a439ba4
Sai Rohit <sairohit11.s@gmail.com>
2024-11-21 13:44:19 +0530

    add env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+if (typeof window !== "undefined") {
+  // checks that we are client-side
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host:
+      process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com",
+    person_profiles: "always", // or 'always' to create profiles for anonymous users as well
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
+    }
+  });
+}
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps }
+}: any) {
+  return (
+    <>
+      <PostHogProvider client={posthog}>
+        <Component {...pageProps} />
+      </PostHogProvider>
+    </>
+  );
 }
